5692d9da84e159e7e0c9eeb71f05dcb7542b9278
despencer <despencer@mail.ru>
2023-08-16 20:34:29 +0300

    Initial coding for sector stream reading.

--- ole/ole.py
+++ ole/ole.py
@@ -1,19 +1,75 @@
 import os
 
+ENDOFCHAIN = 0xFFFFFFFE
+
 class SectorChainStream:
     def __init__(self, meta, datafile):
         self._meta = meta
         self.datafile = datafile
+        self.sectors = None
+        self.pos = 0
 
     def seek(self, delta, postype=os.SEEK_SET):
-        return self.datafile.seek(delta, postype)
+        self.setup()
+        if postype == os.SEEK_END:
+            self.pos = None
+        elif postype == os.SEEK_CUR:
+            self.pos = self.pos + delta
+        else:
+            self.pos = delta
+        self.checkpos()
+        return self.pos
 
     def read(self, size):
-        return self.datafile.read(size)
+        self.setup()
+        acc = bytes()
+        self.acquiresectors(self.pos+size)
+        isect = self.pos // self.posbase
+        self.datafile.seek(self.sectors[isect] + (self.pos % self.posbase) )
+        while size >= 0:
+            acc += self.datafile.read( min(size, self.posbase) )
+            self.pos += min(size, self.posbase)
+            size -= self.posbase
+            isect += 1
+            if isect >= len(self.sectors):
+                break
+            self.datafile.seek(self.sectors[isect])
+        return acc
 
     def __repr__(self):
         return self._meta.prettyprint(self)
 
+    def setup(self):
+        if not self.sectors is None:
+            return
+        self.sectors = [ self.header.firstdifatsect ]
+        self.sectsize = 1 << self.header.sectorshift
+        self.posbase = self.sectsize - 4
+
+    def checkpos(self):
+        if self.pos is None:
+            self.acquiresectors(-1)
+            self.pos = ( len(self.sectors) * self.posbase ) - 1
+        else:
+            if self.pos >= ( len(self.sectors) * self.posbase ):
+                self.getfullchain()
+            if self.pos >= ( len(self.sectors) * self.posbase ):
+                self.pos = ( len(self.sectors) * self.posbase ) - 1
+        if self.pos < 0:
+            self.pos = 0
+
+    def acquiresectors(self, lastpos):
+        if lastpos < 0:
+            maxsect = self.header.numdifatsect+1
+        else:
+            maxsect = lastpos // self.posbase
+        while len(self.sectors) <= maxsect:
+            self.datafile.seek( self.sectors[-1], os.SEEK_SET)
+            sect = self.datafile.read(self.sectsize)
+            nextsect = int.from_bytes(sect[-4:].read(4), 'little')
+            if nextsect == ENDOFCHAIN:
+                raise Exception('Unexpected end of chain')
+            self.sectors.append(nextsect)
 
 class SectorChainStreamReader:
     def __init__(self):
