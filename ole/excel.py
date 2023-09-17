0ef116f6450348a8ac24ee58101c545bc45e03c5
despencer <despencer@mail.ru>
2023-09-17 11:59:36 +0300

    General BIFF8 reader added.

---
+++ ole/excel.py
@@ -0,0 +1,22 @@
+import os
+
+class Biff8Record:
+    def __init__(self, rectype, size):
+        self.rectype = rectype
+        self.size = size
+
+    def __repr__(self):
+        return "BIFF8 {:04X} of size {:04X}".format(self.rectype, self.size)
+
+class Biff8RecordReader:
+    def read(self, datafile):
+        header = datafile.read(4)
+        record = Biff8Record(int.from_bytes(header[0:2], 'little'), int.from_bytes(header[2:4], 'little') )
+        record.raw = datafile.read(record.size)
+        return record
+
+    def readsize(self, datafile):
+        header = datafile.read(4)
+        size = int.from_bytes(header[2:4], 'little')
+        datafile.seek(size, os.SEEK_CUR)
+        return size
